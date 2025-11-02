f0da8eec9e43c22d6166cee522f865647afbf53e
Jeremias Santos <jeremias.steinmetz@gmail.com>
2025-11-02 15:27:22 -0300

    iniciando projeto spring

---
+++ src/main/java/com/goes/crm_backoffice/CrmBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.goes.crm_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goes/crm_backoffice/CrmBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goes.crm_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
